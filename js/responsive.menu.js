14e0efbfb4f199613b91bfb73ace6a3d70c3ab06
accgit <zdenek.papucik@gmail.com>
2019-02-15 14:01:40 +0100

    update

--- js/responsive.menu.js
+++ js/responsive.menu.js
@@ -9,19 +9,26 @@
 		$('.nav .responsive').toggleClass('expandable');
 	});
 
-	// responsive menu
+	// init menu
 	$('ul.menu li').each(function() {
+		$(this).parent().find('li.parent a.employ').addClass('up');
 		if ($(this).children('li.parent a.current').length > 0 ) {
 			$(this).parent().find('ul.submenu').toggle();
-			$(this).parent().find('li.parent a.employ').addClass('active');
+			$(this).parent().find('li.parent a.employ').addClass('active down');
 		}
 	});
 
-	// responsive active menu
+	// active menu
 	$('ul.menu li a').on('click', function() {
 		$(this).parent('li.parent').find('a.employ').toggleClass('active');
 		$(this).parent().find('ul.submenu').slideToggle('fast');
 		$(this).parent().find('ul.submenu ul.submenu').toggle();
+	});
+
+	// click transition
+	$('a.employ').on('click', function() {
+		$(this).addClass('transition');
+		$(this).toggleClass('rotate');
 	});
 
 }(jQuery));
