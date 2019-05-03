8def56869059ea7859c88bb132c05ebf166be5e3
accgit <zdenek.papucik@gmail.com>
2019-05-03 08:49:28 +0200

    Fix and update

--- js/responsive.menu.js
+++ js/responsive.menu.js
@@ -1,34 +1,43 @@
 
-/*
- * Responsive menu.
+/*!
+ * CSS UI - Responsive menu
+ * Copyright Zdeněk Papučík
+ * MIT License
  */
 (function($) {
 
-	// click show/hide menu
+
+	// toggle sidebar menu
 	$('.nav .click').on('click', function() {
 		$('.nav .responsive').toggleClass('expandable');
 	});
 
+
 	// init menu
 	$('ul.menu li').each(function() {
 		$(this).parent().find('li.parent a.employ').addClass('up');
-		if ($(this).children('li.parent a.current').length > 0 ) {
-			$(this).parent().find('ul.submenu').toggle();
-			$(this).parent().find('li.parent a.employ').addClass('active down');
+		if ($(this).find('li.parent a.current').length > 0 ) {
+			$(this).parent().find('a.current').addClass('active down');
+			$(this).parent().find('a.current').next('ul.submenu').show();
 		}
 	});
 
+
 	// active menu
 	$('ul.menu li a').on('click', function() {
-		$(this).parent('li.parent').find('a.employ').toggleClass('active');
-		$(this).parent().find('ul.submenu').slideToggle('fast');
-		$(this).parent().find('ul.submenu ul.submenu').toggle();
+		$(this).parent().find('a.employ').toggleClass('active');
+		$(this).next('ul.submenu').slideToggle('fast');
 	});
+
 
 	// click transition
 	$('a.employ').on('click', function() {
 		$(this).addClass('transition');
 		$(this).toggleClass('rotate');
+		if ($(this).parent().find('a.employ').hasClass('down')) {
+			$(this).toggleClass('rotate-revert');
+		}
 	});
+
 
 }(jQuery));
